6e3ac60bcfc3bd0bfd35b7961f554e380f4ea92d
kamilWnek <kamil.wnek97@gmail.com>
2022-10-10 19:28:11 +0200

    improve displaying activity data

--- src/ActivityDetail.js
+++ src/ActivityDetail.js
@@ -1,8 +1,19 @@
+import { Progress } from "antd";
+import "antd/dist/antd.css";
+
 const ActivityDetail = ({ name, value }) => {
   return (
     <li>
       <p>{name}:</p>
-      <p>{value}</p>
+      {name == "type" || name == "participants" ? (
+        <p>{value}</p>
+      ) : (
+        <Progress
+          percent={value * 100}
+          showInfo={false}
+          status={value === 1 ? "exception" : ""}
+        />
+      )}
     </li>
   );
 };
