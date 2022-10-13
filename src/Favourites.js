d377b034a94c1f5b9ae58a69da88f4a392a947cc
kamilWnek <kamil.wnek97@gmail.com>
2022-10-14 00:02:05 +0200

    add favourites view

--- src/Favourites.js
+++ src/Favourites.js
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import ActivityView from "./ActivityView";
+import { getKeysFromLocalStorage } from "./LocalStorage";
+
 const Favourites = () => {
-  return <div className="fav">Tutaj pojawią się ulubione aktywności</div>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    setActivities(
+      getKeysFromLocalStorage().map((key) =>
+        JSON.parse(window.localStorage.getItem(key))
+      )
+    );
+  }, []);
+
+  const setIsLiked = (isLiked, activity) => {
+    if (!isLiked) {
+      const index = activities.indexOf(activity);
+      const newActiviyies = [...activities];
+      newActiviyies.splice(index, 1);
+      setActivities(newActiviyies);
+    }
+  };
+
+  return (
+    <div className="favourites">
+      {activities.map((activity) => (
+        <ActivityView
+          key={activity.key}
+          activityObject={activity}
+          setIsLiked={setIsLiked}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Favourites;
