06f551881108f22fa2ec14c52204da215b86593f
kamilWnek <kamil.wnek97@gmail.com>
2022-10-21 22:06:56 +0200

    update about content

--- src/About.js
+++ src/About.js
@@ -2,26 +2,28 @@ import cssImg from "./images/css.png";
 import htmlImg from "./images/html.png";
 import jsImg from "./images/js.png";
 import reactImg from "./images/react.png";
-import telImg from "./images/tel.png";
-import mailImg from "./images/mail.png";
-import linkedinImg from "./images/in.png";
-import gitImg from "./images/git.png";
-import messengerImg from "./images/messenger.png";
-import ContactItem from "./ContactItem";
 
 const About = () => {
   return (
     <div className="about">
       <h2>Bored Activities</h2>
       <p>
-        This app was developed while I was learning React. It is my own
-        implementation of{" "}
+        This is my first React app, and it was developed while I was learning to
+        this technology. It is my own implementation of{" "}
         <a
           href="https://www.boredapi.com/documentation#endpoints-price"
           target="_blank"
           rel="noreferrer"
         >
           Bored API.
+        </a>{" "}
+        Check out source code on{" "}
+        <a
+          href="https://github.com/kamilinw/Bored-activities"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub repository
         </a>
       </p>
       <h2>Technologies</h2>
@@ -34,10 +36,13 @@ const About = () => {
       <br />
       <h2>About me</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut
-        euismod ligula, id vehicula libero. Etiam non orci felis. Nunc sed
-        ultricies risus. Maecenas iaculis vitae felis sed bibendum. Sed ut
-        egestas nisl. Nulla iaculis suscipit risus id porta.
+        Programming has always been my passion. Right now I&apos;m software
+        engineer and I&apos;m programming home automation solutions, as well as
+        developing software for electric vehicle charging stations. For some
+        time I&apos;m learning about web technologies. I think that the
+        technical studies I have completed (Mechatronics at the Rzeszów
+        University of Technology), will help me to understand real problems and
+        find their solutions.
       </p>
     </div>
   );
