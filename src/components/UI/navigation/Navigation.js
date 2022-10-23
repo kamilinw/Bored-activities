60baef36dfe41b2c417011b95fb065a2fae6e0c7
kamilWnek <kamil.wnek97@gmail.com>
2022-10-23 23:21:44 +0200

    make logo clickable

--- src/components/UI/navigation/Navigation.js
+++ src/components/UI/navigation/Navigation.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import "antd/dist/antd.css";
 import { Breadcrumb } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Navigation = () => {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/`;
+    navigate(path);
+  };
+
   return (
     <nav className="navigation">
       <div id="logo">
-        <p>Bored activities</p>
+        <Link to="/">Bored activities</Link>
       </div>
       <Breadcrumb>
         <Breadcrumb.Item>
