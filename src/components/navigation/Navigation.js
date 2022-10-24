3a30c03bd8c7bacf8cdd82cdd1f26e2d811d236d
kamilWnek <kamil.wnek97@gmail.com>
2022-10-24 20:55:04 +0200

    reload component when clicking on logo or random activity in navigarion

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
 import "antd/dist/antd.css";
 import { Breadcrumb } from "antd";
-import { useNavigate } from "react-router-dom";
 
 const Navigation = () => {
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = `/`;
-    navigate(path);
-  };
-
   return (
     <nav className="navigation">
       <div id="logo">
-        <Link to="/">Bored activities</Link>
+        <Link to="/" onClick={() => window.location.reload()}>
+          Bored activities
+        </Link>
       </div>
       <Breadcrumb>
         <Breadcrumb.Item>
-          <Link className="link" to="/">
+          <Link
+            className="link"
+            to="/"
+            onClick={() => window.location.reload()}
+          >
             Random activity
           </Link>
         </Breadcrumb.Item>
